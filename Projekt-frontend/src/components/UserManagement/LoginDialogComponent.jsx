0777e7d6219d8b5e517ae1f33d2f3f38951ad5a3
CetinaLuka <luka.cetina@student.um.si>
2021-06-09 17:33:32 +0200

    polish: improved login window

--- Projekt-frontend/src/components/UserManagement/LoginDialogComponent.jsx
+++ Projekt-frontend/src/components/UserManagement/LoginDialogComponent.jsx
@@ -7,6 +7,7 @@ import TextField from '@material-ui/core/TextField';
 import Collapse from '@material-ui/core/Collapse';
 import Button from '@material-ui/core/Button';
 import { withTranslation } from 'react-i18next';
+import {Grid, Typography} from "@material-ui/core";
 
 class LoginDialogComponent extends Component {
     constructor(props) {
@@ -27,47 +28,66 @@ class LoginDialogComponent extends Component {
     render() {
         return (
             <Dialog onClose={this.props.handleLoginDialogClickClose} open={this.props.isShowing}>
-            <form >
-                <TextField id="outlined-basic" required label={this.props.t("userManagement.username")} variant="outlined" style={{ marginTop: "100px", marginLeft: "100px", marginRight: "100px" }}
-                    onChange={(e) => { this.setState({ username: e.target.value }) }} /> <br /> <br />
-                <TextField id="outlined-basic" required type="password" label={this.props.t("userManagement.password")} variant="outlined" style={{ marginTop: "10px", marginBottom: "10px", marginLeft: "100px", marginRight: "100px" }}
-                    onChange={(e) => { this.setState({ password: e.target.value }) }} /><br /> <br />
-                <Button style={{ marginBottom: "20px", marginLeft: "100px", marginRight: "100px" }} variant="contained" color="primary"
-                    onClick={() => {
-                        if (this.state.username === "" || this.state.password === "") {
-                            this.wrongCredentials(true, "Please fill all required fields");
-                            return
-                        }
-                        this.wrongCredentials(false);
-                        UserService.login(this.state.username, this.state.password).then((resp) => {
-                            this.props.logInSuccessful();
-                            const decodedJWT = (UserService.decodeJWT(resp.headers.authorization))
-                            window.sessionStorage.setItem("user", JSON.stringify(
-                                {
-                                user_id: decodedJWT.user_id,
-                            company_id: decodedJWT.company_id,
-                            jwt: resp.headers.authorization
-                        }));
+                <form >
+                    <Grid container maxWidth="lg" justify="center" style={{padding: '20px'}}>
+                        <Grid item xs={12}>
+                            <Typography variant="h6">
+                                Dobrodošli nazaj na eTermin
+                            </Typography>
+                            <Typography variant="body">
+                                Zahvaljujemo se vam za vaše zaupanje in vam želimo uspešen dan na delu 💪.
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12} style={{marginTop: '20px'}}>
+                            <TextField fullWidth={true} id="outlined-basic" required label={this.props.t("userManagement.username")} variant="outlined"
+                                onChange={(e) => { this.setState({ username: e.target.value }) }} /> <br /> <br />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField fullWidth={true} id="outlined-basic" required type="password" label={this.props.t("userManagement.password")} variant="outlined"
+                                onChange={(e) => { this.setState({ password: e.target.value }) }} /><br /> <br />
+                        </Grid>
+                        <Grid item xs={12} style={{textAlign: 'center'}}>
+                            <Button style={{ marginBottom: "20px", marginLeft: "100px", marginRight: "100px" }} variant="contained" color="primary"
+                                onClick={() => {
+                                    if (this.state.username === "" || this.state.password === "") {
+                                        this.wrongCredentials(true, "Please fill all required fields");
+                                        return
+                                    }
+                                    this.wrongCredentials(false);
+                                    UserService.login(this.state.username, this.state.password).then((resp) => {
+                                        this.props.logInSuccessful();
+                                        const decodedJWT = (UserService.decodeJWT(resp.headers.authorization))
+                                        window.sessionStorage.setItem("user", JSON.stringify(
+                                            {
+                                                user_id: decodedJWT.user_id,
+                                                company_id: decodedJWT.company_id,
+                                                jwt: resp.headers.authorization
+                                            }));
 
-                        })
-                            .catch((err) => {
-                                if (err.response)
-                                    this.wrongCredentials(true, err.response.data.reason);
-                                else {
-                                    console.log(err)
-                                }
-                            });
-                    }}>
-                    Log in
-                </Button>
-            </form>
-            <Collapse in={this.state.wrongCredentials}>
-                <Alert severity="error" style={{ marginBottom: "20px", marginLeft: "100px", marginRight: "100px" }}>
-                    <AlertTitle>Error</AlertTitle>
-                    {this.state.wrongCredentials_error}
-                </Alert>
-            </Collapse>
-        </Dialog>
+                                    })
+                                        .catch((err) => {
+                                            if (err.response)
+                                                this.wrongCredentials(true, err.response.data.reason);
+                                            else {
+                                                console.log(err)
+                                            }
+                                        });
+                                }}>
+                                Log in
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+
+
+                </form>
+                <Collapse in={this.state.wrongCredentials}>
+                    <Alert severity="error" style={{ marginBottom: "20px", marginLeft: "100px", marginRight: "100px" }}>
+                        <AlertTitle>Error</AlertTitle>
+                        {this.state.wrongCredentials_error}
+                    </Alert>
+                </Collapse>
+            </Dialog>
         )
     }
 }
