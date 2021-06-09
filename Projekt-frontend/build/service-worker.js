cfcf350336d4a52fdcea97551736b0a978e9a540
PrimozStopar <primoz.stopar@student.um.si>
2021-06-09 20:35:04 +0200

    icons, description

--- Projekt-frontend/build/service-worker.js
+++ Projekt-frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.63aaf1dee6a00e40566384cdb7b73227.js"
+  "./precache-manifest.84bceec18f6521b6cbfe6c9d85ffe079.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
