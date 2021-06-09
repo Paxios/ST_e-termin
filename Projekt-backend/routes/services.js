6db97a2192c6ff14162c8d561b8864eb86cae6d8
PrimozStopar <primoz.stopar@student.um.si>
2021-06-09 21:06:22 +0200

    regex for report

--- Projekt-backend/routes/services.js
+++ Projekt-backend/routes/services.js
@@ -9,7 +9,7 @@ var mongo = require('mongodb')
 router.use(jwt({
     secret: process.env.SECRET,
     algorithms: ['HS256']
-}).unless({ path: ['/user/register', "/user/login", "/storitev/"] }));
+}).unless({ path: [/^\/porocilo\/.*/, '/user/register', "/user/login"] }));
 
 router.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
